5afcac11f3e9c216268c5062fa52ef5ed9130304
Facundo Cabrera <cabrerafacundo@gmail.com>
2014-06-10 19:57:48 -0300

    Added lodash

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/lodash/dist/lodash.js',
 
       'app/scripts/app.js',
       'app/scripts/constants/server.js',
